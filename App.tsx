8d0361644fbe4280c27972970d9d4bbd7981cee2
Gui-dev <adrianon10@gmail.com>
2022-08-01 04:24:06 -0300

    feat: add component Routes

--- App.tsx
+++ App.tsx
@@ -4,7 +4,7 @@ import { NativeBaseProvider } from 'native-base'
 import { useFonts, Roboto_400Regular, Roboto_700Bold } from '@expo-google-fonts/roboto'
 
 import { THEME } from './src/styles/theme'
-import { SignIn } from './src/screens/SignIn'
+import { Routes } from './src/routes'
 import { Loading } from './src/components/Loading'
 
 export default function App () {
@@ -24,7 +24,7 @@ export default function App () {
   return (
     <NativeBaseProvider theme={THEME}>
       <StatusBar style="light" backgroundColor="transparent" translucent/>
-      <SignIn />
+      <Routes />
     </NativeBaseProvider>
   )
 }
