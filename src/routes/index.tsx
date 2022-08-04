8cdd676666c486ef0d64372dd14bb98509fb954d
Gui-dev <adrianon10@gmail.com>
2022-08-04 03:12:04 -0300

    feat: add check if user is logged in

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,11 +1,29 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import { NavigationContainer } from '@react-navigation/native'
+import { onAuthStateChanged, User } from 'firebase/auth'
+
+import { firebaseAuth } from './../services/firebase'
 
 import { SignIn } from './../screens/SignIn'
 import { AppRoutes } from './app.routes'
+import { Loading } from '../components/Loading'
 
 export const Routes = () => {
-  const user = true
+  const [loading, setLoading] = useState(true)
+  const [user, setUser] = useState<User>()
+
+  useEffect(() => {
+    const subscriber = onAuthStateChanged(firebaseAuth, response => {
+      setUser(response)
+      setLoading(false)
+    })
+
+    return subscriber
+  }, [])
+
+  if (loading) {
+    return <Loading />
+  }
 
   return (
     <NavigationContainer>
